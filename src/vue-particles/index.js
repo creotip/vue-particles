5c12b7f43f8796e984f90fcc0a9bfad617000b16
Fourat Janabi <fouratj@gmail.com>
2018-04-01 11:16:32 -0300

    :repeat: Fix eslint issues

--- src/vue-particles/index.js
+++ src/vue-particles/index.js
@@ -1,13 +1,9 @@
-/* eslint-disable */
 import particles from './vue-particles.vue'
 
 const VueParticles = {
-
-    install (Vue, options) {
-        Vue.component('vue-particles', particles)
-    }
-
+  install (Vue, options) {
+    Vue.component('vue-particles', particles)
+  }
 }
 
 export default VueParticles
-/* eslint-disable */
