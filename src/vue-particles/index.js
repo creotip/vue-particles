de70afe27226ce9ffe66f445bcb9a994143c0a09
ancampaci <andrea.campaci@objectway.com>
2018-11-22 21:45:48 +0100

    fix: IE11 + SCRIPT1003: Expected ‘:’ error

--- src/vue-particles/index.js
+++ src/vue-particles/index.js
@@ -3,7 +3,7 @@ import particles from './vue-particles.vue'
 
 const VueParticles = {
 
-    install (Vue, options) {
+    install: function(Vue, options) {
         Vue.component('vue-particles', particles)
     }
 
